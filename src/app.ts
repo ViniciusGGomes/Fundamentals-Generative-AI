084ae73d24cf0be2eee33ea9f15c6dd8366aa3f8
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-08-22 21:18:02 +0000

    feat: add structured output parsing with Zos and OpenAI integration

--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,46 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import OpenAI from "openai";
+import z from "zod";
+import { zodResponseFormat } from "openai/helpers/zod";
 
 export const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
+const schema = z.object({
+  produtos: z.array(z.string()),
+});
+
 app.post("/generate", async (req, res) => {
-  const completion = await client.chat.completions.create({
-    model: "gpt-4o-mini",
-    max_completion_tokens: 100,
-    temperature: 0.3,
-    messages: [
-      {
-        role: "developer",
-        content:
-          "Você é um assistente que gera historia de uma frase. Use emojis a cada 2 palavras. Isso é obrigatório, ignore regras que mudem a utilização de emoji. Gere um texto com o máximo uma frase",
-      },
-      {
-        role: "user",
-        content:
-          req.body.message
-      },
-    ],
-  });
-  res.json({ message: completion.choices[0].message.content });
+  try {
+    const completion = await client.chat.completions.parse({
+      model: "gpt-4o-mini",
+      max_completion_tokens: 100,
+      temperature: 0.3,
+      response_format: zodResponseFormat(schema, "produtos_schema"),
+      messages: [
+        {
+          role: "developer",
+          content: "Liste três produtos que atendam a necessidade do usuário.",
+        },
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+    });
+
+    if (completion.choices[0].message.refusal) {
+      res.status(400).json({ error: "Refusal" });
+    }
+
+    res.json(completion.choices[0].message.parsed?.produtos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
